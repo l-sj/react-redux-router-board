1cfe2580b5f6a57b36e3b79350b263daa28c5d94
seojinDev <>
2018-08-07 20:40:31 +0900

    다시하기

--- src/pages/BoardViewPage.js
+++ src/pages/BoardViewPage.js
@@ -4,13 +4,21 @@ import { Link } from 'react-router-dom'
 import BoardViewContent from '../components/BoardViewContent'
 
 export default class BoardViewPage extends Component {
-  // constructor(){
-  //   super();
-  // }
+  constructor(){
+    super();
+    this.state = {
 
-  // prevGoBack() {
-  //   this.props.history.goBack();
-  // }
+    }
+  }
+
+  prevGoBack() {
+    this.props.history.goBack();
+  }
+
+  componentDidMount(){
+    console.log('ViePage history -------: ', window.history.state);
+    console.log('ViePage-------: ', window.history.state.state.id);
+  }
 
   render() {
     console.log('View this.props.history: ', this.props.history);
@@ -28,7 +36,7 @@ export default class BoardViewPage extends Component {
               <Link to="/" className="btn btn-outline-dark btn-sm">목록</Link>
             </div>
             <div className="btn-group" role="group" aria-label="Second group">
-              <Link to="/board_update" className="btn btn-outline-dark btn-sm">수정</Link>
+              <Link to={`/board_update/${window.history.state.state.id}`} className="btn btn-outline-dark btn-sm">수정</Link>
               <button type="button" className="btn btn-outline-dark btn-sm">삭제</button>
             </div>
           </div>
