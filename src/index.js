7c6fad82c2df7b4a0aa4866f9cd0c99f750eb676
seojinDev <>
2018-08-07 20:45:59 +0900

    처음부터 다시

--- src/index.js
+++ src/index.js
@@ -27,6 +27,14 @@ if (module.hot) {
     store.replaceReducer(require('./reducers'))
   );
 }
+/*
+replaceReducer(nextReducer)
+  현재 스토어에서 상태를 계산하기 위해 사용중인 리듀서를 교체한다.
+  이것은 고급 API로 코드 분할이나 동적으로 리듀서를 불러오고 싶을때 사용할 수 있다.
+  Redux에서 핫 리로딩을 구현하기 위해서도 사용할 수 있다.
+  전달인자
+    reducer:(Function)스토어가 앞으로 사용할 리듀서
+*/
 
 ReactDOM.render(
   <Provider store={store}>
@@ -38,12 +46,17 @@ ReactDOM.render(
           <Route path='/board_create' component={ BoardCreatePage} />
           <Route path='/board_update' component={ BoardUpdatePage } />
         </App> */}
-        <Route exact path="/" component={ BoardListPage } />
-        <Route path="/board_view" component={ BoardViewPage } />
+        
+        <Route exact path="/" render={(props) => (
+          <BoardListPage {...props} />
+        )} />
         <Route path="/board_create" render={(props) => (
           <BoardCreatePage {...props} />
         )} />
-        <Route path="/board_update" render={(props) => (
+        <Route path="/board_view/:id" render={(props) => (
+          <BoardViewPage {...props} />
+        )} />
+        <Route path="/board_update/:id" render={(props) => (
           <BoardUpdatePage {...props} />
         )} />
       </Switch>
